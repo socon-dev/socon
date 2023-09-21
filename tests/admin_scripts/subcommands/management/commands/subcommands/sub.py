cafba89f068cc90b319eeb83ccef1e4e06fa0a87
Stephane Capponi <stephane.capponi@gmail.com>
2023-09-21 09:03:50 +0200

    [#42] Fix subcommand config attribute

--- tests/admin_scripts/subcommands/management/commands/subcommands/sub.py
+++ tests/admin_scripts/subcommands/management/commands/subcommands/sub.py
@@ -20,3 +20,13 @@ class SubTwocommand(BaseCommand):
 
     def handle(self, config: Config) -> None:
         print(config.extras_args)
+
+
+class SubAccessConfig(BaseCommand):
+    """Subcommand to check access to sub config attribute"""
+
+    name = "sub-config"
+    manager = "with_subs"
+
+    def handle(self, config: Config) -> None:
+        assert self.config == config
